68512197fc16d59f75532865c61396d009828638
Amr Gharaba <amr_ibr@live.com>
2023-11-07 14:55:37 +0200

    added assignment

---
+++ _python/python_fundamentals/Hello_world.py
@@ -0,0 +1,24 @@
+print('Hello world')
+#############
+string = 'Amr'
+print('Your name : '+ string)
+#############
+print('Your name : ', string)
+#############
+number = 42
+print('Hello '+ str(42))
+#############
+print('Hello',42)
+#############
+print('Hello '+ str(42))
+#############
+food_one = 'burgers'
+food_two = "Pizza"
+#############
+print('i love eating {} and {}'.format(food_one,food_two))
+#############
+print(f'i love eating {food_one} and {food_two}')
+#############
+print('i love eating %s and %s'%(food_one, food_two))
+
+
