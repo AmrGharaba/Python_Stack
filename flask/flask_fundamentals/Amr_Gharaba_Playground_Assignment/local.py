0774264b17da3d0b753c9c33b9ecad4c4b3dec30
Amr Gharaba <amr_ibr@live.com>
2023-11-12 14:19:00 +0200

    added flsk assignment

---
+++ flask/flask_fundamentals/Amr_Gharaba_Playground_Assignment/local.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/play/')
+def playGround():
+    return render_template('index.html', num = 3, color = 'hsl(214.38deg 86.41% 79.8%)' )
+@app.route('/play/<num>')
+def playGroundNum(num):
+    if(num == None):
+        return render_template('index.html')
+    return render_template('index.html', num = int(num), color = 'hsl(214.38deg 86.41% 79.8%)')
+@app.route('/play/<num>/<color>')
+def playGroundColor(num, color):
+    if(num == None):
+        return render_template('index.html')
+    return render_template('index.html', num = int(num), color = color)
+
+if __name__ =="__main__":
+    app.run(debug=True)
--- flask/flask_fundamentals/Amr_Gharaba_Understanding_Routing_Assignment/routing.py
+++ flask/flask_fundamentals/Amr_Gharaba_Understanding_Routing_Assignment/routing.py
@@ -17,3 +17,4 @@ def repeat(num, str):
     return strs
 if __name__=="__main__":
     app.run(debug=True)
+else: print('sorry')
