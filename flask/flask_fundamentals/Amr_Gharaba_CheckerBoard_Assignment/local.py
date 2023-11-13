6f9c9bdeb7e005d5d220a2fde91ffe5114031de1
Amr Gharaba <amr_ibr@live.com>
2023-11-13 13:35:48 +0200

    added flask checkerboard assignment

--- flask/flask_fundamentals/Amr_Gharaba_CheckerBoard_Assignment/local.py
+++ flask/flask_fundamentals/Amr_Gharaba_CheckerBoard_Assignment/local.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def main():
-    return render_template('index.html',x=int(8), y = int(8), color1 = 'red', color2 = 'blue')
+    return render_template('index.html',x=8, y = 8, color1 = 'red', color2 = 'blue')
 @app.route('/<y>')
 def main_y(y):
-    return render_template('index.html',x=int(8), y = int(y), color1 = 'red', color2 = 'blue')
+    return render_template('index.html',x=8, y = y, color1 = 'red', color2 = 'blue')
 @app.route('/<y>/<x>')
 def main_yx(y,x):
     return render_template('index.html',x=int(x), y = int(y), color1 = 'red', color2 = 'blue')
