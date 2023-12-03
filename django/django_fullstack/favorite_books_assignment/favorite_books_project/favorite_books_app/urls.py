5433af96ab8d43b569439e6ef4f6efdbacab65a0
Amr Gharaba <amr_ibr@live.com>
2023-12-03 13:02:56 +0200

    updated assignment

--- django/django_fullstack/favorite_books_assignment/favorite_books_project/favorite_books_app/urls.py
+++ django/django_fullstack/favorite_books_assignment/favorite_books_project/favorite_books_app/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('logout', views.logout),
     path('add_book',views.add_book),
     path('book_details/<id>',views.book_details),
-    path('unfavorite/<id>', views.unfavorite)
+    path('unfavorite/<id>', views.unfavorite),
+    path('add_to_favorite/<id>',views.add_to_favorite),
+    path('update/<id>',views.update),
+    path('delete/<id>',views.delete),
 ]
--- django/django_fullstack/favorite_books_assignment/favorite_books_project/favorite_books_app/views.py
+++ django/django_fullstack/favorite_books_assignment/favorite_books_project/favorite_books_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib import messages
 from .models import User ,Book
+import bcrypt
 
 def root(request):
     return render(request,'index.html')
@@ -15,7 +16,8 @@ def processing(request):
                 messages.error(request, value)
             return redirect('/')
         else:
-            User.objects.create(first_name = request.POST['first_name'],last_name = request.POST['last_name'],email_address = request.POST['email_address'],password = request.POST['password'])
+            hash_pass = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+            User.objects.create(first_name = request.POST['first_name'],last_name = request.POST['last_name'],email_address = request.POST['email_address'],password = hash_pass)
             request.session['id'] = User.objects.last().id
             request.session['action'] = 'Registered'
             messages.success(request, "User created Successfully")
@@ -23,7 +25,10 @@ def processing(request):
 
 
     elif request.POST['which_form'] == 'login':
-        if User.objects.filter(email_address = request.POST['email_address']) and User.objects.get(email_address = request.POST['email_address']).password == request.POST['password']:
+        print(User.objects.get(email_address = request.POST['email_address']).password)
+        print('input password : ',bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode())
+        
+        if User.objects.filter(email_address = request.POST['email_address']) and bcrypt.checkpw(request.POST['password'].encode(), User.objects.get(email_address = request.POST['email_address']).password.encode()):
             request.session['id'] = User.objects.get(email_address = request.POST['email_address']).id
             request.session['action'] = 'Logged in'
             return redirect('/success')
@@ -61,13 +66,32 @@ def add_book(request):
 def book_details(request,id):
     content = {
         'book': Book.objects.get(id = id),
-        'fav_users':User.objects.filter(fav_books = Book.objects.get(id = id))
+        'fav_users':User.objects.filter(fav_books = Book.objects.get(id = id)),
+        'current_user' : User.objects.get(id = request.session['id']),
     }
-    
-    if User.objects.get(books = Book.objects.get(id = id)).id == request.session['id']:
-        content['display'] = 'show'
-    else: content['display'] = 'none'
     return render(request,'book_details.html',content)
+
 def unfavorite(request,id):
     Book.objects.get(id = id).fav_users.remove(User.objects.get(id = request.session['id']))
+    return redirect('/success')
+
+def add_to_favorite(request,id):
+    User.objects.get(id = request.session['id']).fav_books.add(Book.objects.get(id = id))
+    return redirect('/success')
+
+def update(request, id):
+    
+    errors = Book.objects.basic_validator(request.POST)
+    if len(errors) > 0 :
+            for key, value in errors.items():
+                messages.error(request, value)
+    else:
+        book_update = Book.objects.get(id = id)
+        book_update.desc = request.POST['desc']
+        book_update.save()
+    return redirect(f'/book_details/{Book.objects.get(id = id).id}')
+
+def delete(request, id):
+    book_delete = Book.objects.get(id = id)
+    book_delete.delete()
     return redirect('/success')
